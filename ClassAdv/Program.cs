f75d100cac9b15ed7a3485cfd9aca2a6778697c3
seungchae <s2125@e-mirim.hs.kr>
2023-06-27 09:08:41 +0900

    소멸자 실습

--- ClassAdv/Program.cs
+++ ClassAdv/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,17 @@ namespace ClassAdv
     {
         static void Main(string[] args)
         {
+            // 소멸자 실습
+            Product product1 = new Product("과자", 1000);
+            Product product2 = new Product("빼빼로", 1000);
+            Product product3 = new Product("닭꼬치", 1000);
+            Product product4 = new Product("탕후루", 1000);
+
+            Console.WriteLine("안녕하세요");
+            Console.WriteLine("안녕하세요");
+            Console.WriteLine("안녕하세요");
+            Console.WriteLine("안녕하세요");
+            Console.WriteLine("안녕하세요");
         }
     }
 }
